b57fd09f6e6063c43a8597d026d6e6d2cd064f1b
Jonah Meijers <jonahmeijers97@gmail.com>
2021-03-22 11:43:45 +0100

    feat: add background synchronisation

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,11 +3,28 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/serviceWorker.js', {
-        updateViaCache: 'all',
+      .register('/serviceWorker.js', { updateViaCache: 'all' })
+      .then(() => navigator.serviceWorker.ready)
+      .then(reg => {
+        navigator.serviceWorker.addEventListener('message', e => {
+          if ('timestamp' in e.data)
+            localStorage.setItem('build-timestamp', e.data.timestamp)
+        })
+        const timestamp = localStorage.getItem('build-timestamp')
+        if (timestamp) reg.active?.postMessage({ timestamp })
+        if ('periodicSync' in reg)
+          //@ts-expect-error: Periodic sync is supported in some browsers, but not typed
+          return reg.periodicSync.getTags().then((tags: string[]) => {
+            if (!tags.includes('sync-pages'))
+              //@ts-expect-error: Periodic sync is supported in some browsers, but not typed
+              reg.periodicSync.register('sync-pages', {
+                minInterval: 24 * 60 * 60 * 1000,
+              })
+          })
+        else if ('sync' in reg) return reg.sync.register('sync-pages')
+        // @ts-expect-error: This line is a fallback in case a browser doesn't support (periodic) background syncing
+        reg.active.postMessage({ sync: true })
       })
-      .then(() => {
-        console.log('Service worker registered')
-      })
+      .catch(console.error)
   })
 }
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -7,9 +7,7 @@ const CORE_CACHE = 'henkercore'
 const PAGE_CACHE = 'henkerpage'
 const ASSET_CACHE = 'henkerasset'
 const CORE_CACHE_URLS = ['/offline', '/styles/main.css', '/scripts/main.js']
-let coreCacheName = CORE_CACHE
-let pageCacheName = PAGE_CACHE
-let assetCacheName = ASSET_CACHE
+let timestamp = ''
 
 // Solution for type problems from: https://github.com/Microsoft/TypeScript/issues/11781#issuecomment-785350836
 const sw: ServiceWorkerGlobalScope & typeof globalThis = self as any
@@ -17,8 +15,7 @@ const sw: ServiceWorkerGlobalScope & typeof globalThis = self as any
 sw.addEventListener('install', e => {
   e.waitUntil(
     (async () => {
-      await updateCacheNames()
-      const cache = await caches.open(coreCacheName)
+      const cache = await caches.open(CORE_CACHE)
       await cache.addAll(CORE_CACHE_URLS)
       await sw.skipWaiting()
     })(),
@@ -29,15 +26,14 @@ sw.addEventListener('activate', e => {
   sw.clients.claim()
   e.waitUntil(
     (async () => {
-      await updateCacheNames()
       const keys = await caches.keys()
       await Promise.all(
         keys
           .filter(
             key =>
-              (key.includes(PAGE_CACHE) && key !== pageCacheName) ||
-              (key.includes(CORE_CACHE) && key !== coreCacheName) ||
-              (key.includes(ASSET_CACHE) && key !== assetCacheName),
+              (key.includes(PAGE_CACHE) && key !== PAGE_CACHE) ||
+              (key.includes(CORE_CACHE) && key !== CORE_CACHE) ||
+              (key.includes(ASSET_CACHE) && key !== ASSET_CACHE),
           )
           .map(caches.delete),
       )
@@ -47,23 +43,44 @@ sw.addEventListener('activate', e => {
 
 sw.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return e.respondWith(fetch(e.request))
-  caches.match(e.request, { ignoreSearch: true }).then(cacheRes => {
-    if (cacheRes) return e.respondWith(cacheRes)
-    const url = new URL(e.request.url)
-    if (url.pathname.includes('/story/') || url.pathname === '/')
-      return e.respondWith(addToCache(pageCacheName, e.request))
-    if (CORE_CACHE_URLS.includes(url.pathname))
-      return e.respondWith(addToCache(coreCacheName, e.request))
-    return e.respondWith(addToCache(assetCacheName, e.request))
-  })
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(cacheRes => {
+      if (cacheRes) return cacheRes
+      const url = new URL(e.request.url)
+      if (url.pathname.includes('/story/') || url.pathname === '/')
+        return addToCache(PAGE_CACHE, e.request)
+      if (CORE_CACHE_URLS.includes(url.pathname))
+        return addToCache(CORE_CACHE, e.request)
+      return addToCache(ASSET_CACHE, e.request)
+    }),
+  )
 })
 
-async function updateCacheNames() {
+sw.addEventListener('message', e => {
+  if ('timestamp' in e.data) timestamp = e.data.timestamp
+  if ('sync' in e.data) e.waitUntil(synchronisePages)
+})
+
+sw.addEventListener('sync', e => {
+  if (e.tag === 'sync-pages') e.waitUntil(synchronisePages)
+})
+
+// @ts-expect-error: Periodic sync is an event that is supported, but not typed
+sw.addEventListener('periodicsync', (e: SyncEvent) => {
+  if (e.tag === 'sync-pages') e.waitUntil(synchronisePages)
+})
+
+async function synchronisePages() {
   const res = await fetch('/version')
-  const version = await res.text()
-  coreCacheName = `${CORE_CACHE}-${version}`
-  pageCacheName = `${PAGE_CACHE}-${version}`
-  assetCacheName = `${ASSET_CACHE}-${version}`
+  const newTimestamp = await res.text()
+  if (timestamp !== newTimestamp) {
+    timestamp = newTimestamp
+    sw.postMessage({ timestamp })
+    const cache = await caches.open(PAGE_CACHE)
+    const reqs = await cache.keys()
+    await Promise.all(reqs.map(req => addToCache(PAGE_CACHE, req)))
+    await sw.skipWaiting()
+  }
 }
 
 async function addToCache(name: string, req: Request): Promise<Response> {
